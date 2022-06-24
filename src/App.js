d97aff9c87097e4005f7ad9c2976f979d69198b4
Aniket Rath <75676123+Aniket-Rath@users.noreply.github.com>
2022-06-23 21:57:09 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./MyComponents/Header";
+import Footer from "./MyComponents/Footer"
+import Todos from "./MyComponents/Todos"
+import React, {useState} from "react"
+
+function App() 
+{
+  
+  const onDelete = (todo) =>
+  {
+    console.log("Are you sure ?",todo);
+    setTodos(todos.filter((e) => {
+      return e!== todo;
+    } ))
+  }
+
+
+  const [todos, setTodos] = useState([
+
+    {
+      sno:1,
+      title: "Go to Market.",
+      desc:"We need to Shop for  Something",
+    },
+    {
+      sno:2,
+      title: "Go to Mall.",
+      desc:"We need to Shop for  Something",
+    },
+    {
+      sno:3,
+      title: "Return Home.",
+      desc:"We need to Return Home",
+    }
+  ]);
+    return (
+        <>
+            <Header title=" My Todo's List | " searchBar ={false}/>
+            <Todos todos={todos} onDelete={onDelete}/>
+            <Footer/>
+        </>
+    );
+}
+
+export default App;
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+
+  let footerStyle ={
+    position: "absolute",
+    top: "100vh",
+    width: "100%"
+  }
+ 
+  return (
+    <Footer className="bg-dark text-light py-3" style={footerStyle}>
+      <p className="text-center">
+        Copyright &copy; Aniket_rath
+      </p>
+    </Footer>
+  )
+}
+
+export default Footer;
